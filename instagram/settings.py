8433910f0819008a8653b622afe9ac568d6b4b1f
monginadiana <diana.ondieki@student.moringa.com>
2021-12-02 08:17:04 +0300

    addition of the database password

--- instagram/settings.py
+++ instagram/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gram',
+        'USER': 'access',
+    'PASSWORD':'1234',
     }
 }
 
