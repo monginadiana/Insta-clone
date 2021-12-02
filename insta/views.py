a8458fa5b2e5c088661ddb36e25d5496ece17ad2
monginadiana <diana.ondieki@student.moringa.com>
2021-12-02 18:50:19 +0300

    addition of the imports in the views

--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import Post
+from .models import *
 
 # Create your views here.
 
-def home(request):
+def index(request):
     images = Post.objects.all().order_by('-id')
  
     return render(request, 'index.html', {'images': images})
