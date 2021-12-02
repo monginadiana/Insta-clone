eb430f8745b885c526e0dc72a6cf6cd7e013f417
monginadiana <diana.ondieki@student.moringa.com>
2021-12-02 18:47:42 +0300

    addition of the views for the post class

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'all-clone/index.html')
+def home(request):
+    images = Post.objects.all().order_by('-id')
+ 
+    return render(request, 'index.html', {'images': images})
