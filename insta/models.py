678b7c0b20e8a8898c0cc96bc5efc4f50e870815
monginadiana <diana.ondieki@student.moringa.com>
2021-12-03 15:03:46 +0300

    addition of the class profile

--- insta/models.py
+++ insta/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Post(models.Model):
-  
+    
 
     image = CloudinaryField('image')
     image_name = models.CharField(max_length=50)
     image_caption = models.TextField(max_length=100)
     image_date = models.DateTimeField(auto_now_add=True)
+    
+    @classmethod
+    def get_images_by_user(cls, user):
+        images = cls.objects.filter(user=user)
+        return images
 
     def save_image(self):
         self.save()
@@ -22,5 +27,32 @@ class Post(models.Model):
         self.image_caption = new_caption
         self.save()
     
+      # search images using image name
+    @classmethod
+    def search_by_image_name(cls, search_term):
+        images = cls.objects.filter(
+            image_name__icontains=search_term)
+        return images
+
+    #  get a single image using id
+    @classmethod
+    def get_single_image(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
     def __str__(self):
         return self.image_name
+
+
+    
+    def __str__(self):
+        return self.image_name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_photo = CloudinaryField('image')
+
+    bio = models.TextField(max_length=500, blank=True, null=True)
+
+    contact = models.CharField(max_length=50, blank=True, null=True)
--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from .models import *
-
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import *
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -9,3 +11,18 @@ def index(request):
     images = Post.objects.all().order_by('-id')
  
     return render(request, 'all-clone/index.html', {'images': images})
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+    if request.method == 'POST':
+        image_name = request.POST['image_name']
+        image_caption = request.POST['image_caption']
+        image_file = request.FILES['image_file']
+        image_file = cloudinary.uploader.upload(image_file)
+        image_url = image_file['url']
+        image = Post(image_name=image_name, image_caption=image_caption, image=image_url,
+                      profile_id=request.POST['user_id'], user_id=request.POST['user_id'])
+        image.save_image()
+        return redirect('/profile', {'success': 'Successfully posted'})
+    else:
+        return render(request, 'profile.html', {'danger': 'posting Failed'})
