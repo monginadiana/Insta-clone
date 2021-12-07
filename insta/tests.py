6050cf867d2064a7b73162dd75dcb5f1fe945cc1
monginadiana <diana.ondieki@student.moringa.com>
2021-12-07 11:27:06 +0300

    addition of the profile testcases

--- insta/tests.py
+++ insta/tests.py
@@ -18,13 +18,27 @@ class PostTestCase(TestCase):
         )
         Post.objects.create(
             image_name='test_image',
-            image='https://doographics.com/assets/dg/images/cooking-youtube-thumbnail/cooking',
+            image='https://unsplash.com/photos/Pp6efQ_ghiA',
             image_caption='test image',
-            profile_id=user.id,
-            user_id=user.id
         )
     def test_image_name(self):
         image = Post.objects.get(image_name='test_image')
         self.assertEqual(image.image_name, 'test_image')
 
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username='test_user',
+            first_name='diana',
+            last_name='mongina'
+        )
+        Profile.objects.create(
+            bio='test bio',
+            profile_photo='https://unsplash.com/photos/Pp6efQ_ghiA',
+            user_id=user.id
+        )
+
+    def test_bio(self):
+        profile = Profile.objects.get(bio='test bio')
+        self.assertEqual(profile.bio, 'test bio')
 
